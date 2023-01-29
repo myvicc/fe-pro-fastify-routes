55d0188c98d8dd0fb48a00b10e47a89c06350efa
myvicc <dir@acg.co.ua>
2023-01-29 23:00:29 +0200

    resolving

--- src/index.js
+++ src/index.js
@@ -12,3 +12,51 @@ fastify.register(import('@fastify/multipart'), {
 fastify.register(import('@fastify/cookie'));
 
 export default fastify;
+
+fastify.post("/uppercase", (request, reply) => {
+  const word = "fuck";
+  const answer = "unresolved";
+  const { body } = request;
+  if (body.toLowerCase().includes(word.toLowerCase())) {
+    reply.status(403);
+    return answer;
+  }
+    reply.status(200);
+    return body.toUpperCase();
+})
+
+fastify.post("/lowercase", (request, reply) => {
+  const word = "fuck";
+  const { body } = request;
+
+  if (body.toLowerCase().includes(word.toLowerCase())) {
+    reply.status(403);
+    return {
+      message: "unresolved"
+    }
+  }
+    reply.status(200);
+    return body.toLowerCase();
+
+})
+
+const routeUser = "/user"
+fastify.get(`${routeUser}/:id`, (request, reply) => {
+  const { id } = request.params;
+  const user = Object.keys(users).find((idUser) => +idUser === id);
+  if (user) {
+    return user;
+  }
+    reply.status(400);
+    return {
+      message: "User not exist"
+    }
+})
+
+fastify.get("/users", (request) => {
+  const { filter, value } = request.query;
+  if (filter && value) {
+    return Object.values(users).filter((user) => user.hasOwnProperty(filter) && user[filter] === value );
+  } else
+    return Object.values(users);
+})
